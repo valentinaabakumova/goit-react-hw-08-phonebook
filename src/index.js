cd0d78458f082ab5d8992c88e614243bd4d43a6b
valentinaabakumova <abakumova.va13@gmail.com>
2022-11-16 00:07:39 +0100

    +basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <LangProvider> */}
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
